770f31b6cfb946262abe28d3cb661978ae27eea7
Arjun Chakravarthy Nunugonda <anunugonda@portware.com>
2021-04-13 16:28:33 +0530

    Added springdoc

--- src/main/java/com/sherlock/herokupoc/spring/core/RestControllers.java
+++ src/main/java/com/sherlock/herokupoc/spring/core/RestControllers.java
@@ -2,11 +2,14 @@ package com.sherlock.herokupoc.spring.core;
 
 import java.io.File;
 import java.nio.charset.Charset;
+import java.text.CharacterIterator;
 import java.text.SimpleDateFormat;
+import java.text.StringCharacterIterator;
 import java.util.Calendar;
 
 import org.apache.commons.io.FileUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +21,7 @@ public class RestControllers {
 	private static final String ORIGINS = "*";
 
 	@CrossOrigin(origins = ORIGINS)
-	@RequestMapping(value = "/logAlert")
+	@GetMapping(value = "/logAlert")
 	public String logAlert(@RequestParam(value = "hostname", required = true) String hostname,
 			@RequestParam(value = "content", required = true) String content) throws Exception {
 		Gson gson = new Gson();
@@ -32,7 +35,7 @@ public class RestControllers {
 	}
 
 	@CrossOrigin(origins = ORIGINS)
-	@RequestMapping(value = "/getAlert")
+	@GetMapping(value = "/getAlert")
 	public String getAlert(@RequestParam(value = "hostname", required = true) String hostname) throws Exception {
 		Gson gson = new Gson();
 		File file = getAlertFile(hostname);
@@ -40,26 +43,60 @@ public class RestControllers {
 				.toString()));
 	}
 
-	private static File getAlertFile(String hostname) {
-		return new File("test-temp/", hostname + "alerts.log");
+	@CrossOrigin(origins = ORIGINS)
+	@GetMapping(value = "/space")
+	public String space() throws Exception {
+		Gson gson = new Gson();
+		String contextPath = System.getProperty("user.dir");
+		File contextDirectory = new File(contextPath);
+		return gson.toJson(calculateAvailableSpace(contextDirectory));
 	}
 
 	@CrossOrigin(origins = ORIGINS)
-	@RequestMapping(value = "/statusCheck")
+	@GetMapping(value = "/statusCheck")
 	public String getStatusCheck() throws Exception {
 		return "OK";
 	}
 
 	@CrossOrigin(origins = ORIGINS)
-	@RequestMapping(value = "handshake")
+	@GetMapping(value = "handshake")
 	public String handshake() throws Exception {
 		return "OK";
 	}
 
 	@CrossOrigin(origins = ORIGINS)
-	@RequestMapping(value = "status")
+	@GetMapping(value = "status")
 	public String status() throws Exception {
 		return "OK";
+	}
+
+	/*
+	 * UTILS
+	 */
+	private static File getAlertFile(String hostname) {
+		return new File("test-temp/", hostname + "alerts.log");
+	}
+
+	private static String calculateAvailableSpace(File targetLocation) throws Exception {
+		long usableSpace = targetLocation.getUsableSpace();
+		long totalSpace = targetLocation.getTotalSpace();
+
+		return humanReadableByteCountBin(usableSpace) + " / " + humanReadableByteCountBin(totalSpace);
+	}
+
+	private static String humanReadableByteCountBin(long bytes) {
+		long absB = bytes == Long.MIN_VALUE ? Long.MAX_VALUE : Math.abs(bytes);
+		if (absB < 1024) {
+			return bytes + " B";
+		}
+		long value = absB;
+		CharacterIterator ci = new StringCharacterIterator("KMGTPE");
+		for (int i = 40; i >= 0 && absB > 0xfffccccccccccccL >> i; i -= 10) {
+			value >>= 10;
+			ci.next();
+		}
+		value *= Long.signum(bytes);
+		return String.format("%.3f %ciB", value / 1024.0, ci.current());
 	}
 
 }
